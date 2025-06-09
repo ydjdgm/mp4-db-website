1bd3fcc347416451bb07bc325532ad0edc928c19
ydjdgm <tonytony060720@gmail.com>
2025-06-09 19:16:58 +0200

    .

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,4 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<video src="Roll-a-Ball-Win-Edited.mp4" />
-<video src="Roll-a-Ball-Lose-Edited.mp4" />
+<video src="Roll-a-Ball-GamePlay.mp4" />
